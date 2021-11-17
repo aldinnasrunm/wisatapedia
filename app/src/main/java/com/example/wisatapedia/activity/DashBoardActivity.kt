b91b8be68cf41a3ad8ce4fd8c4414bee010f893c
aldinnasrunm <adnzain40@gmail.com>
2021-11-18 05:48:21 +0700

    we got navbar fully worked. next add logic in each fragment

--- app/src/main/java/com/example/wisatapedia/activity/DashBoardActivity.kt
+++ app/src/main/java/com/example/wisatapedia/activity/DashBoardActivity.kt
@@ -2,8 +2,14 @@ package com.example.wisatapedia.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.wisatapedia.R
 import com.example.wisatapedia.databinding.ActivityDashBoardBinding
+import com.example.wisatapedia.view.HomeFragment
+import com.example.wisatapedia.view.SearchFragment
+import com.example.wisatapedia.view.SettingFragment
+import com.example.wisatapedia.view.WhislistFragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
 private lateinit var binding : ActivityDashBoardBinding
 class DashBoardActivity : AppCompatActivity() {
@@ -14,7 +20,44 @@ class DashBoardActivity : AppCompatActivity() {
         setView()
     }
 
-    private fun setView(){
-        binding.tvWelcome.text = "Hello"
+
+    private fun setView() {
+        val MainFragment = HomeFragment()
+        addFragment(MainFragment)
+        val navbar = binding.bottomNavigationViewMain
+        navbar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    val fragment = HomeFragment()
+                    addFragment(fragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.search -> {
+                    val fragment = SearchFragment()
+                    addFragment(fragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.whislist -> {
+                    val fragment = WhislistFragment()
+                    addFragment(fragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.setting -> {
+                    val fragment = SettingFragment()
+                    addFragment(fragment)
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> false
+            }
+        }
     }
+    fun addFragment(fragment : Fragment){
+        val mainFrag = R.id.fragment_main
+        supportFragmentManager
+            .beginTransaction()
+            .replace(mainFrag, fragment, fragment.javaClass.simpleName)
+            .commit()
+    }
+
 }
--- app/src/main/java/com/example/wisatapedia/view/HomeFragment.kt
+++ app/src/main/java/com/example/wisatapedia/view/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.wisatapedia.fragment
+package com.example.wisatapedia.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
