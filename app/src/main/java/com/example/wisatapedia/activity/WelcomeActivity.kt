558c66f853ef676833d7f7fe3c5652647a7729e8
icet <clarisca2003.cap@gmail.com>
2021-11-18 15:51:53 +0700

    fixing RegisterActivity.kt

--- app/src/main/java/com/example/wisatapedia/activity/WelcomeActivity.kt
+++ app/src/main/java/com/example/wisatapedia/activity/WelcomeActivity.kt
@@ -3,10 +3,13 @@ package com.example.wisatapedia.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.wisatapedia.R
+import com.example.wisatapedia.databinding.ActivityWelcomeBinding
 
+private lateinit var binding: ActivityWelcomeBinding
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
